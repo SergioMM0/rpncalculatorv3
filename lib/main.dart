1da9707096f217760702ecbb3f94242ba3447267
Sergio M <serg0971@easv365.dk>
2023-03-02 12:11:35 +0100

    actual state

--- lib/main.dart
+++ lib/main.dart
@@ -8,9 +8,10 @@ class CalculatorApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.dark(),
-      themeMode: ThemeMode.dark,
-      home: MainScreen());
+        theme: ThemeData.dark(),
+        themeMode: ThemeMode.dark,
+        title: 'RPN Calculator',
+        home: MainScreen());
   }
 }
 
@@ -19,14 +20,53 @@ class MainScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('RPN Calculator'),
-      ),
-      body: Text('wow')
 
+    var _input = '2';
+    var _stack = [2.0,3.0];
+
+    return Scaffold(
+      body: Container(
+        color: Colors.grey,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Container(
+              height: MediaQuery.of(context).size.height * 0.20,
+              width: MediaQuery.of(context).size.width,
+              color: Colors.white,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  FittedBox(
+                    fit: BoxFit.fill,
+                    child: Container(
+                      margin: EdgeInsets.only(left: 10,top: 10, right: 10),
+                      alignment: Alignment.centerRight,
+                      color: Colors.green,
+                      child: Text('$_input'),
+                    ),
+                  ),
+                  FittedBox(
+                    fit: BoxFit.fill,
+                      child: Container(
+                        margin: EdgeInsets.all(10),
+                        alignment: Alignment.centerLeft,
+                        color: Colors.blue,
+                        child: Text('$_stack'),
+                      )
+                  )
+                ],
+              ),
+            ),
+            Container(
+              color: Colors.black,
+              height: 60,
+            )
+          ],
+        ),
+      ),
     );
   }
 }
-
-
