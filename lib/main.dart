02be0f536c64debf90228646cea11dbee2e12040
Sergio M <serg0971@easv365.dk>
2023-03-02 15:13:09 +0100

    Maybe?

--- lib/main.dart
+++ lib/main.dart
@@ -20,49 +20,59 @@ class MainScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     var _input = '2';
-    var _stack = [2.0,3.0];
+    var _stack = [2.0, 3.0];
 
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Calculator'),
+      ),
       body: Container(
-        color: Colors.grey,
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.end,
           children: [
-            Container(
-              height: MediaQuery.of(context).size.height * 0.20,
-              width: MediaQuery.of(context).size.width,
-              color: Colors.white,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                mainAxisSize: MainAxisSize.max,
-                children: [
-                  FittedBox(
-                    fit: BoxFit.fill,
-                    child: Container(
-                      margin: EdgeInsets.only(left: 10,top: 10, right: 10),
-                      alignment: Alignment.centerRight,
-                      color: Colors.green,
-                      child: Text('$_input'),
-                    ),
+            Row(
+              children: [
+                Container(
+                  child: SizedBox(
+                    width: 70,
+                    height: 70,
+                    child: TextButton(
+                      style: TextButton.styleFrom(
+                        primary: Colors.orange,
+                        onSurface: Colors.red
+                      ),
+                      onPressed: null,
+                      child: Text('1'),
+                    )
+                    ,
                   ),
-                  FittedBox(
-                    fit: BoxFit.fill,
-                      child: Container(
-                        margin: EdgeInsets.all(10),
-                        alignment: Alignment.centerLeft,
-                        color: Colors.blue,
-                        child: Text('$_stack'),
-                      )
-                  )
-                ],
+                )
+              ],
+            ),
+
+            // TEXT
+            Container(
+              child: Padding(
+                padding: EdgeInsets.only(right: 10),
+                child: Text('20,30,123,123,123,124344444',
+                    style: TextStyle(
+                        fontSize: 30,
+                    )
+                ),
               ),
+              alignment: Alignment(1.0,1.0),
             ),
             Container(
-              color: Colors.black,
-              height: 60,
+              child: Padding(
+                padding: EdgeInsets.all(10),
+                child: Text('10',
+                style: TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold
+                ),),
+              ),
+              alignment: Alignment(1.0,1.0),
             )
           ],
         ),
