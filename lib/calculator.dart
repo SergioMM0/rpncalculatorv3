8f8d8683e68d178a912263baf5b6934656ecde44
Sergio M <serg0971@easv365.dk>
2023-03-03 09:20:36 +0100

    gui finished

--- lib/calculator.dart
+++ lib/calculator.dart
@@ -23,7 +23,7 @@ class Calculator extends StatefulWidget {
 }
 
 class _CalculatorState extends State<Calculator> {
-  String display = '222';
+  String display = '';
 
   void buttonPressed(String buttonText) {
     setState(() {
@@ -49,13 +49,17 @@ class _CalculatorState extends State<Calculator> {
             height: 16.0,
           ),
           Row(
-            mainAxisAlignment: MainAxisAlignment.end,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             mainAxisSize: MainAxisSize.max,
             children: [
+              CustomElevatedButton.orangeGradient(
+                child: const Text('Delete'), onPressed: () => buttonPressed('delete'),
+              ),
               Padding(
-                padding: EdgeInsets.only(right: MediaQuery.of(context).size.width * 0.102),
+                padding: EdgeInsets.only(left: MediaQuery.of(context).size.width * 0.37),
+                //To ask: How can I style it in a row. Should I include ghost widgets in the row?
                 child: CustomElevatedButton.orangeGradient(
-                    child: const Text('/'), onPressed: () {}
+                    child: const Text('/'), onPressed: () => buttonPressed('/'),
                 ),
               ),
             ],
@@ -67,13 +71,13 @@ class _CalculatorState extends State<Calculator> {
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               CustomElevatedButton.orangeGradient(
-                  child: const Text('7'), onPressed: () {}),
+                  child: const Text('7'), onPressed: () => buttonPressed('7')),
               CustomElevatedButton.orangeGradient(
-                  child: const Text('8'), onPressed: () {}),
+                  child: const Text('8'), onPressed: () => buttonPressed('8')),
               CustomElevatedButton.orangeGradient(
-                  child: const Text('9'), onPressed: () {}),
+                  child: const Text('9'), onPressed: () => buttonPressed('9')),
               CustomElevatedButton.orangeGradient(
-                  child: const Text('x'), onPressed: () {}),
+                  child: const Text('x'), onPressed: () => buttonPressed('x')),
             ],
           ),
           const SizedBox(
@@ -83,13 +87,13 @@ class _CalculatorState extends State<Calculator> {
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               CustomElevatedButton.orangeGradient(
-                  child: const Text('4'), onPressed: () {}),
+                  child: const Text('4'), onPressed: () => buttonPressed('4')),
               CustomElevatedButton.orangeGradient(
-                  child: const Text('5'), onPressed: () {}),
+                  child: const Text('5'), onPressed: () => buttonPressed('5')),
               CustomElevatedButton.orangeGradient(
-                  child: const Text('6'), onPressed: () {}),
+                  child: const Text('6'), onPressed: () => buttonPressed('6')),
               CustomElevatedButton.orangeGradient(
-                  child: const Text('-'), onPressed: () {}),
+                  child: const Text('-'), onPressed: () => buttonPressed('-')),
             ],
           ),
           const SizedBox(
@@ -99,29 +103,29 @@ class _CalculatorState extends State<Calculator> {
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               CustomElevatedButton.orangeGradient(
-                  child: const Text('1'), onPressed: () {}),
+                  child: const Text('1'), onPressed: () => buttonPressed('1')),
               CustomElevatedButton.orangeGradient(
-                  child: const Text('2'), onPressed: () {}),
+                  child: const Text('2'), onPressed: () => buttonPressed('2')),
               CustomElevatedButton.orangeGradient(
-                  child: const Text('3'), onPressed: () {}),
+                  child: const Text('3'), onPressed: () => buttonPressed('3')),
               CustomElevatedButton.orangeGradient(
-                  child: const Text('+'), onPressed: () {}),
+                  child: const Text('+'), onPressed: () => buttonPressed('+')),
             ],
           ),
           const SizedBox(
-            height: 40.0,
+            height: 16.0,
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               CustomElevatedButton.orangeGradient(
-                  child: const Text('Undo'), onPressed: () {}),
+                  child: const Text('Undo'), onPressed: () => buttonPressed('undo')),
               CustomElevatedButton.orangeGradient(
-                  child: const Text('0'), onPressed: () {}),
+                  child: const Text('0'), onPressed: () => buttonPressed('0')),
               CustomElevatedButton.orangeGradient(
-                  child: const Text('.'), onPressed: () {}),
+                  child: const Text('.'), onPressed: () => buttonPressed('.')),
               CustomElevatedButton.orangeGradient(
-                  child: const Text('Enter'), onPressed: () {}),
+                  child: const Text('Enter'), onPressed: () => buttonPressed('enter')),
             ],
           ),
         ],
